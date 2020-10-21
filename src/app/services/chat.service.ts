c7ff46d000bc49240b5593bd79708e7f76a257d8
Jonathan Perry <jpper@umich.edu>
2020-10-21 14:44:59 +0900

    Finished up tutorial

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/auth";
 import { AngularFirestore } from "@angular/fire/firestore";
 import * as firebase from "firebase/app";
+import { Observable } from "rxjs";
+import { switchMap, map } from "rxjs/operators";
 
 export interface User {
   uid: string;
@@ -57,5 +59,40 @@ export class ChatService {
       from: this.currentUser.uid,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
     });
+  }
+
+  getChatMessages() {
+    let users = [];
+    return this.getUsers().pipe(
+      switchMap((res) => {
+        users = res;
+        console.log("all users: ", users);
+        return this.afs
+          .collection("messages", (ref) => ref.orderBy("createdAt"))
+          .valueChanges({ idField: "id" }) as Observable<Message[]>;
+      }),
+      map((messages) => {
+        for (let m of messages) {
+          m.fromName = this.getUserForMsg(m.from, users);
+          m.myMsg = this.currentUser.uid === m.from;
+        }
+        return messages;
+      })
+    );
+  }
+
+  getUsers() {
+    return this.afs
+      .collection("users")
+      .valueChanges({ idField: "uid" }) as Observable<User[]>;
+  }
+
+  getUserForMsg(msgFromId, users: User[]): string {
+    for (let usr of users) {
+      if (usr.uid == msgFromId) {
+        return usr.email;
+      }
+    }
+    return "Deleted";
   }
 }
